a9e86959ccb61fe0b9d3202827dcc119b92e08ec
Alejandro Martinez <alejomartinez8@gmail.com>
2021-05-19 20:03:28 -0500

    Add PORT

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(process.env.PORT || 4000);
 }
