7da9c823f2cb0e7e976dd5145c0980d7ccb9ea47
Alejandro Martinez <alejomartinez8@gmail.com>
2021-06-11 21:05:25 -0500

    feat(cors): configure enableCors & whitelist

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const whitelist = [
+  'http://localhost:3000',
+  'https://data-warehouse-am.netlify.app/',
+];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: function (origin, callback) {
+      console.log(origin);
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 4000);
 }
