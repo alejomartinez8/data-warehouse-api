1ce86e9f3cbe732b38a0d368705c89f27529b507
Alejandro Martinez <alejomartinez8@gmail.com>
2021-07-08 21:24:09 -0500

    Add domain to whitelist (#9)

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 const whitelist = [
   'http://localhost:3000',
   'https://data-warehouse-am.herokuapp.com',
+  'https://data-warehouse-app.vercel.app',
 ];
 
 async function bootstrap() {
