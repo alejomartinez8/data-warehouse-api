06d4e0d040137f149c65838fa7c9263af11fd5db
Alejandro Martinez <alejomartinez8@gmail.com>
2021-08-09 00:03:19 -0500

    Reconfigure cookies

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,21 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-const whitelist = [
-  'http://localhost:3000',
-  'https://data-warehouse-am.herokuapp.com',
-  'https://data-warehouse-app.vercel.app',
-];
+// const whitelist = [
+//   'http://localhost:3000',
+//   'https://data-warehouse-am.herokuapp.com',
+//   'https://data-warehouse-app.vercel.app',
+// ];
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: false,
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
+    // allowedHeaders:
+    //   'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    // methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    // credentials: true,
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 4000);
